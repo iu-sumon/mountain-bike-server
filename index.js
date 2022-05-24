fe5fcedbde08fa12c20cb94b8ba1d10149e6d9b9
Md. Sumon Mia <md.sumonmia.cse.iu@gmail.com>
2022-05-25 03:20:18 +0600

    all parts api added

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express')
 const cors = require('cors')
 const port = process.env.PORT || 5000;
@@ -22,6 +22,7 @@ async function run() {
         const brandsCollection = client.db('mountain-bicycle').collection('brands')
         const partsCollection = client.db('mountain-bicycle').collection('parts')
         const reviewsCollection = client.db('mountain-bicycle').collection('reviews')
+        const ordersCollection = client.db('mountain-bicycle').collection('orders')
 
         //====================================== Get all brands loading API
 
@@ -30,7 +31,7 @@ async function run() {
             const brands = await brandsCollection.find(query).toArray();
             res.send(brands)
         })
-        
+
         //====================================== Get all Reviews loading API
 
         app.get('/reviews', async (req, res) => {
@@ -46,15 +47,44 @@ async function run() {
             res.send(parts)
         })
 
+        //=======================================Single Parts data loading API
+
+        app.get('/purchasePage/:id', async (req, res) => {
+
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const part = await partsCollection.findOne(query);
+            res.send(part)
 
 
+        })
+        //===================================== Deleting Order API
+        app.delete('/order/:id', async (req, res) => {
 
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await ordersCollection.deleteOne(query);
+            res.send(result)
 
+        })
+        //======================================Booking API
+        app.post('/order', async (req, res) => {
 
+            const order = req.body;
+            const result = await ordersCollection.insertOne(order);
+            res.send(result)
+        })
 
+        //====================================== My Order API 
 
+        app.get('/order', async (req, res) => {
 
+            const email = req.query.email;
+            const query = { email: email };
+            const orders = await ordersCollection.find(query).toArray();
+            return res.send(orders);
 
+        })
 
 
 
