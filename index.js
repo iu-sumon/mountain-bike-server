4a612b6ac820b2c20c020e0bb89e3d519f579f26
Md. Sumon Mia <md.sumonmia.cse.iu@gmail.com>
2022-05-25 12:34:45 +0600

    all reviews api added

--- index.js
+++ index.js
@@ -75,21 +75,32 @@ async function run() {
             const result = await ordersCollection.insertOne(order);
             res.send(result)
         })
+
         //====================================== Review Added API
+
         app.post('/reviews', async (req, res) => {
 
             const review = req.body;
             const result = await reviewsCollection.insertOne(review);
             res.send(result)
         })
-        //====================================== Profile Added API
-        app.post('/profiles', async (req, res) => {
 
-            const profile = req.body;
-            const result = await profilesCollection.insertOne(profile);
+
+        //====================================== update profile API
+
+        app.put('/profile', async (req, res) => {
+
+            const email = req.query.email;
+            const updatedProfile = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: updatedProfile
+            };
+            const result = await profilesCollection.updateOne(filter, updatedDoc, options)
             res.send(result)
-        })
 
+        })
         //====================================== My Order API 
 
         app.get('/order', async (req, res) => {
