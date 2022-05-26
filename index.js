0902f0a5c009524ff6a78160c96356d7a2456702
Md. Sumon Mia <md.sumonmia.cse.iu@gmail.com>
2022-05-26 13:06:24 +0600

    admin part added

--- index.js
+++ index.js
@@ -49,153 +49,6 @@ async function run() {
         const profilesCollection = client.db('my_profile').collection('profiles')
 
 
-        //====================================== Get all brands loading API
-
-        app.get('/brands', async (req, res) => {
-            const query = {};
-            const brands = await brandsCollection.find(query).toArray();
-            res.send(brands)
-        })
-
-        //====================================== Get all Reviews loading API
-
-        app.get('/reviews', async (req, res) => {
-            const query = {};
-            const reviews = await reviewsCollection.find(query).toArray()
-            res.send(reviews)
-        })
-        //====================================== Get all Parts loading API
-
-        app.get('/parts', async (req, res) => {
-            const query = {};
-            const parts = (await partsCollection.find(query).toArray()).reverse();
-            res.send(parts)
-        })
-        //====================================== Get all Parts loading API
-
-        app.get('/orders', async (req, res) => {
-            const query = {};
-            const orders =await ordersCollection.find(query).toArray()
-            res.send(orders)
-        })
-
-        //======================================= Get all users  Loading Api
-
-        app.get('/users', verifyJWT, async (req, res) => {
-
-            const users = await userCollection.find().toArray();
-            res.send(users);
-        });
-
-        //=======================================Single Parts data loading API
-
-        app.get('/purchasePage/:id', async (req, res) => {
-
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const part = await partsCollection.findOne(query);
-            res.send(part)
-
-
-        })
-        //===================================== Deleting Order API
-        app.delete('/order/:id', async (req, res) => {
-
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const result = await ordersCollection.deleteOne(query);
-            res.send(result)
-
-        })
-        //===================================== Deleting Order API
-        app.delete('/part/:id', async (req, res) => {
-
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const result = await partsCollection.deleteOne(query);
-            res.send(result)
-
-        })
-        //======================================Order added API
-        app.post('/order', async (req, res) => {
-
-            const order = req.body;
-            const result = await ordersCollection.insertOne(order);
-            res.send(result)
-        })
-
-        //====================================== Review Added API
-
-        app.post('/reviews', async (req, res) => {
-
-            const review = req.body;
-            const result = await reviewsCollection.insertOne(review);
-            res.send(result)
-        })
-
-        //====================================== Parts Added API
-
-        app.post('/parts', async (req, res) => {
-
-            const part = req.body;
-            const result = await partsCollection.insertOne(part);
-            res.send(result)
-        })
-
-
-        //====================================== update profile API
-
-        app.put('/profile', async (req, res) => {
-
-            const email = req.query.email;
-            const updatedProfile = req.body;
-            const filter = { email: email };
-            const options = { upsert: true };
-            const updatedDoc = {
-                $set: updatedProfile
-            };
-            const result = await profilesCollection.updateOne(filter, updatedDoc, options)
-            res.send(result)
-
-        })
-        //====================================== My Order API 
-
-        app.get('/order', verifyJWT, async (req, res) => {
-
-            const email = req.query.email;
-            const query = { email: email };
-            const orders = await ordersCollection.find(query).toArray();
-            return res.send(orders);
-        })
-
-        //======================================= Make a admin API
-
-        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
-
-            const email = req.params.email;
-            const requester = req.decoded.email; // je onno user k admin banate chay  tar email neya hosche  
-            const requesterAccount = await userCollection.findOne({ email: requester }); // sei email diye sei user k khuje ber kora hosche 
-
-            if (requesterAccount.role === 'admin') { // admin requester user a jodi role property thake tahole se onno user k admin banate parbe
-
-                const filter = { email: email };
-                const updateDoc = {
-                    $set: { role: 'admin' },
-                };
-
-                const result = await userCollection.updateOne(filter, updateDoc);
-                res.send(result);
-
-            }
-
-            else {
-                res.status(403).send({ message: 'forbidden' });
-            }
-
-        });
-
-
-
         //====================================New and old User checking api (JWT main API)
 
         app.put('/user/:email', async (req, res) => {
@@ -214,6 +67,198 @@ async function run() {
             const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
             res.send({ result, token })
         });
+
+
+
+        //========================================== Verify Admin 
+
+        const verifyAdmin = async (req, res, next) => {
+
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: requester });
+            if (requesterAccount.role === 'admin') {
+                next();
+            }
+            else {
+                res.status(403).send({ message: 'forbidden' });
+            }
+        }
+
+        //===================================== Admin route protected API  . ,
+
+        app.get('/admin/:email', async (req, res) => {
+
+            const email = req.params.email;
+
+            const user = await userCollection.findOne({ email: email });
+
+            const isAdmin = user.role === 'admin';
+
+            res.send({ admin: isAdmin })
+
+        })
+
+        //======================================= Make a admin API
+
+        app.put('/user/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
+
+            const email = req.params.email;
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: requester });
+
+            if (requesterAccount.role === 'admin') {
+
+                const filter = { email: email };
+                const updateDoc = {
+                    $set: { role: 'admin' },
+                };
+
+                const result = await userCollection.updateOne(filter, updateDoc);
+                res.send(result);
+
+            }
+
+            else {
+                res.status(403).send({ message: 'forbidden' });
+            }
+
+        });
+
+        //====================================== Get all brands loading API
+
+        app.get('/brands', async (req, res) => {
+            const query = {};
+            const brands = await brandsCollection.find(query).toArray();
+            res.send(brands)
+        })
+
+        //====================================== Get all Reviews  API
+
+        app.get('/reviews', async (req, res) => {
+            const query = {};
+            const reviews = await reviewsCollection.find(query).toArray()
+            res.send(reviews)
+        })
+        //====================================== Post Review  API
+
+        app.post('/reviews', async (req, res) => {
+
+            const review = req.body;
+            const result = await reviewsCollection.insertOne(review);
+            res.send(result)
+        })
+
+
+
+        //====================================== Get all Parts API
+
+        app.get('/parts', async (req, res) => {
+            const query = {};
+            const parts = (await partsCollection.find(query).toArray()).reverse();
+            res.send(parts)
+        })
+        //=======================================Get Single Parts data l API
+
+        app.get('/purchasePage/:id', async (req, res) => {
+
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const part = await partsCollection.findOne(query);
+            res.send(part)
+
+
+        })
+        //====================================== Parts post API (admin)
+
+        app.post('/parts', verifyJWT, verifyAdmin, async (req, res) => {
+
+            const part = req.body;
+            const result = await partsCollection.insertOne(part);
+            res.send(result)
+        })
+        //===================================== Parts delete API (admin)
+        app.delete('/part/:id', verifyJWT, verifyAdmin, async (req, res) => {
+
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await partsCollection.deleteOne(query);
+            res.send(result)
+
+        })
+
+
+
+
+
+        //====================================== Get Orders  API
+
+        app.get('/orders', async (req, res) => {
+            const query = {};
+            const orders = await ordersCollection.find(query).toArray()
+            res.send(orders)
+        })
+        //====================================== My Orders API 
+
+        app.get('/order', verifyJWT, async (req, res) => {
+
+            const email = req.query.email;
+            const decodedEmail = req.decoded.email;
+
+            if (email === decodedEmail) {
+
+                const query = { email: email };
+                const orders = await ordersCollection.find(query).toArray();
+                return res.send(orders);
+            }
+            else {
+                return res.status(403).send({ message: 'Forbidden access' })
+            }
+
+        })
+        //======================================Post Order API
+        app.post('/order', async (req, res) => {
+
+            const order = req.body;
+            const result = await ordersCollection.insertOne(order);
+            res.send(result)
+        })
+        //===================================== Delete Order API
+        app.delete('/order/:id', async (req, res) => {
+
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await ordersCollection.deleteOne(query);
+            res.send(result)
+
+        })
+        //======================================= Get all users  Loading Api
+
+        app.get('/users', verifyJWT, async (req, res) => {
+
+            const users = await userCollection.find().toArray();
+            res.send(users);
+        });
+
+
+
+        //====================================== update profile API
+
+        app.put('/profile', async (req, res) => {
+
+            const email = req.query.email;
+            console.log(email);
+            const profile = req.body;
+            const filter = { email: email };
+            console.log(filter);
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: profile
+            };
+            const result = await profilesCollection.updateOne(filter, updatedDoc, options)
+            res.send(result)
+
+        })
+
 
 
     }
